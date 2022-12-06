d4e7b2e36d9782da4ce517d17aba2aca23d4cd69
FACHRI ARDHIAN <19572003@stmik.jayakarta.ac.id>
2022-12-06 21:38:46 +0700

    uas-movie

---
+++ app/src/main/java/com/example/fachri08/services/ServiceBuilder.kt
@@ -0,0 +1,21 @@
+package com.example.fachri08.services
+
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.themoviedb.org/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
+
